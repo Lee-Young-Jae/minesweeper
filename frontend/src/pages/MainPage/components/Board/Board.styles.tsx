ef88a8e2406274891b778a2311c1d02db26622ac
Lee-Young-Jae <yjlee1937@naver.com>
2023-12-22 17:48:28 +0900

    feat: 지뢰찾기 게임 UI를 구현한다.

--- frontend/src/pages/MainPage/components/Board/Board.styles.tsx
+++ frontend/src/pages/MainPage/components/Board/Board.styles.tsx
@@ -1,19 +1,97 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const StyledContainer = styled.div`
-  display: flex;
-  flex-direction: column;
+  margin: 0 auto;
+  width: fit-content;
+  padding: ${(props) => {
+    return props.theme.padding.l;
+  }};
+
+  border-radius: ${(props) => {
+    return props.theme.borderRadius.s;
+  }};
+
+  background-color: ${(props) => {
+    return props.theme.colors.light;
+  }};
+`;
+
+const StyledTitle = styled.h1`
+  margin: 0;
+  padding: 0;
+  font-size: ${(props) => {
+    return props.theme.fontSize.l;
+  }};
+  color: #fff;
+  text-align: center;
 `;
 
 const StyledRow = styled.div`
   display: flex;
-  flex-direction: row;
+  width: 100%;
 `;
 
-const StyledCell = styled.div`
-  width: 30px;
-  height: 30px;
+interface StyledCellProps {
+  $isRevealed: boolean;
+  $neighbour: number;
+  $isFlag: boolean;
+}
+
+const StyledCell = styled.div<StyledCellProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+
   border: 1px solid black;
+  border-radius: 2px;
+
+  width: 20px;
+  height: 20px;
+  background-color: #eeeeee;
+  opacity: 0.5;
+
+  // isRevealed
+  ${(props) =>
+    props.$isRevealed &&
+    css`
+      background-color: #fff;
+      opacity: 1;
+      cursor: default;
+      font-size: ${(props) => {
+        return props.theme.fontSize.s;
+      }};
+    `}
+
+  // neighbour
+  color: ${(props) => {
+    // 공개 되었을 때
+    if (props.$isRevealed) {
+      switch (props.$neighbour) {
+        case 1:
+          return props.theme.nearMineColors.one;
+        case 2:
+          return props.theme.nearMineColors.two;
+        case 3:
+          return props.theme.nearMineColors.three;
+        case 4:
+          return props.theme.nearMineColors.four;
+        case 5:
+          return props.theme.nearMineColors.five;
+        case 6:
+          return props.theme.nearMineColors.six;
+        case 7:
+          return props.theme.nearMineColors.seven;
+        case 8:
+          return props.theme.nearMineColors.eight;
+        default:
+          return "black";
+      }
+    } else {
+      // return "transparent";
+      return "black";
+    }
+  }};
 `;
 
-export { StyledContainer, StyledRow, StyledCell };
+export { StyledContainer, StyledRow, StyledCell, StyledTitle };
---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,49 @@
+const theme = {
+  colors: {
+    default: "#FF4438",
+    light: "#FFE6E4",
+  },
+  fontSize: {
+    xs: `0.75rem`,
+    sm: `0.875rem`,
+    base: `1rem`,
+    lg: `1.125rem`,
+    xl: `1.25rem`,
+    "2xl": `1.5rem`,
+    "3xl": `1.875rem`,
+  },
+  fontWeight: {
+    normal: "400",
+    bold: "700",
+  },
+  nearMineColors: {
+    one: "#60718F",
+    two: "#8090A9",
+    three: "#C9D1D8",
+    four: "#F0EAE4",
+    five: "#F4D3D1",
+    six: "#F1BDC4",
+    seven: "#F0A6B0",
+    eight: "#FF4438",
+  },
+
+  padding: {
+    s: "8px",
+    m: "16px",
+    l: "24px",
+  },
+
+  margin: {
+    s: "8px",
+    m: "16px",
+    l: "24px",
+  },
+
+  borderRadius: {
+    s: "4px",
+    m: "8px",
+    l: "12px",
+  },
+};
+
+export default theme;
